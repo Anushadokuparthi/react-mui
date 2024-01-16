79f90f83650730a801e0d57063ea3945951257f4
Anusha Dokuparthi <Anusha.Dokuparthi@ivycomptech.com>
2024-01-16 11:41:18 +0530

    Added banner card and footer

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import { Box } from "@mui/material";
-import Card from '@mui/material/Card';
+import { Box, Card, CardContent, Typography } from "@mui/material";
 
 const Banner = () => {
-    return (
-        <Card>
-            <Box
-                style={{
-                    backgroundImage: 'url("https://images.pexels.com/photos/3860805/pexels-photo-3860805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', // url("../assets/banner.jpg")
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    minHeight: '500px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: 'white', // Adjust text color based on your image
-                }}
-            ></Box>
-            <h1>hello</h1>
-        </Card>
+    return (        
+        <Box
+            style={{
+                backgroundImage: 'url("https://images.pexels.com/photos/3860805/pexels-photo-3860805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', // url("../assets/banner.jpg")
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                minHeight: '500px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: 'white', // Adjust text color based on your image
+            }}
+        >
+        <Card sx={{}}>
+            <CardContent>
+                <Box sx={{display:'flex', flexDirection:'column'}}>
+                    <Typography variant='h5'>
+                        One of Saturn's largest rings may be newer than anyone
+                    </Typography>
+                    <Typography variant='body1'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit bibendum vehicula. Donec urna lorem, tincidunt vel porttitor quis, viverra id eros. Aenean quis finibus lacus. Vivamus commodo massa quis urna dignissim placerat. Mauris vestibulum vestibulum quam a finibus. Suspendisse at viverra elit. Proin tortor ante, tempus in mollis sit amet, lacinia sit amet augue. Suspendisse ut rhoncus risus. Nulla eget sodales diam.
 
+Suspendisse potenti. Nam auctor justo luctus risus eleifend varius. Nullam accumsan eget lacus eu pulvinar. Pellentesque pulvinar quis lorem non eleifend. Vestibulum vitae nibh orci. Mauris id enim at odio tempor imperdiet. Praesent eu purus sapien. Proin lorem velit, lacinia ac sodales at, commodo quis eros. Aliquam viverra, elit a vestibulum ornare, tellus turpis varius ex, eget facilisis nunc nisl interdum ex. Vestibulum aliquam accumsan leo, ac porttitor tellus tempus lacinia. Vestibulum porttitor finibus eros vitae consectetur.
+
+Maecenas tellus purus, placerat eu rhoncus vitae, consequat at augue. Pellentesque sit amet orci eu velit venenatis tincidunt. Integer maximus lacus posuere enim lacinia facilisis. Quisque at metus laoreet, gravida nisl eu, commodo mi. Etiam mattis id odio id posuere. Aliquam lacus lorem, bibendum at fringilla et, molestie ac nulla. Quisque condimentum felis ex, sit amet feugiat turpis finibus vitae. Suspendisse tincidunt justo eu cursus semper. Integer vehicula sapien dui, ut ornare nulla accumsan nec. Vestibulum ultrices ex ac dolor feugiat, quis aliquet ex rutrum. Cras interdum sagittis augue sed aliquam.
+
+Donec varius ante vulputate, sodales leo sed, luctus ex. Vivamus ante purus, consectetur at eleifend vel, congue et sapien. Duis ut tempus est. Aliquam et mattis ligula, id tempus augue. Curabitur nec elementum nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi cursus nunc non lectus ultrices, ac tincidunt nisl tincidunt.
+                    </Typography>
+                </Box>                    
+            </CardContent>
+        </Card>
+        </Box> 
     );
 };
 
--- src/components/MailBox.tsx
+++ src/components/MailBox.tsx
@@ -1,27 +1,29 @@
 import { Box, Container, FilledInput, FormControl, IconButton, InputAdornment, InputLabel, Typography } from "@mui/material"
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const MailBox = () => {
-    return(
-        <Container maxWidth="md" sx={{bgcolor:'secondary.main'}}>
-            <Box 
-            sx={{display: 'flex',justifyContent:'space-between', backgroundColor:'secondary', padding: '50px', margin: '40px 0'}} 
-            borderRadius={12}>
+    return (
+        <Container maxWidth="md" sx={{ bgcolor: 'secondary.main', borderRadius: '8px' }}>
+            <Box
+                sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'secondary', padding: '50px', margin: '40px 0 70px 0' }} >
                 <Typography variant="h4" color={"white"}>Lorem ipsum Lorem ipsum Lorem ipsum</Typography>
-                <FormControl sx={{ m: 1, width: '60%' }} variant="filled">
-                <InputLabel htmlFor="filled-adornment-password">Enter Your Email</InputLabel>
-                <FilledInput
-                    id="filled-adornment-password"
-                    type={'email'}
-                    endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton>
-                        <ArrowForwardIcon/>
-                        </IconButton>
-                    </InputAdornment>
-                    }
-                />
-                </FormControl>
+                <Box sx={{width:'60%',backgroundColor:'secondary.light', padding:'10px', borderRadius:'6px'}}>
+                    <FormControl sx={{ width: '100%', mb: 0 }} variant="filled">
+                        <InputLabel htmlFor="filled-adornment-password">Enter Your Email</InputLabel>
+                        <FilledInput sx={{backgroundColor:'white', border:'none', borderRadius:'4px'}}
+                            id="filled-adornment-password"
+                            type={'email'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton>
+                                        <ArrowForwardIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </Box>
             </Box>
         </Container>
-    )}
+    )
+}
 export default MailBox;
